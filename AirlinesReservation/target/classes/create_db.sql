7f2a38ce52b489aed7794d22d57c0e179345aa2c
VigneswarM <imvigneswar@gmail.com>
2019-06-02 18:29:16 -0400

    Removing IDE specific files

--- AirlinesReservation/target/classes/create_db.sql
+++ AirlinesReservation/target/classes/create_db.sql
@@ -9,3 +9,11 @@ insert into admin_logins
 values ('admin01', 'admin1111');
 
 ----------------ADMIN LOGIN --------------------------
+
+----------------USER LOGIN --------------------------
+CREATE TABLE public.user_logins
+(
+    user_id varchar(50),
+    user_email varchar(50) PRIMARY KEY NOT NULL,
+    user_password varchar(50) NOT NULL
+);
