839d85e961f3d48cc3fc0ad84bb2bc206903890d
allurisriakhil <allurisriakhil@gmail.com>
2019-06-08 15:56:14 -0400

    Adding Class Person.java

---
+++ AirlinesReservation/src/main/java/AirlinesApp/AirlinesDAOLayer/user/Person.java
@@ -0,0 +1,30 @@
+
+
+public class Person {
+
+	private String firstName;
+	private String lastName;
+	
+	public Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Person() {}
+
+	public String getfirstName() {
+		return firstName;
+	}
+	
+	public void setfirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getlastName() {
+		return lastName;
+	}
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
