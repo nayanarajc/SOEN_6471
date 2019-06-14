7e258d7c26d3790fd4064ee59a03ecce4c63ab96
VigneswarM <imvigneswar@gmail.com>
2019-06-13 21:24:15 -0400

    DB scripts | Admin Controller -  flights functionalities

--- AirlinesReservation/src/main/resources/create_db.sql
+++ AirlinesReservation/src/main/resources/create_db.sql
@@ -20,5 +20,12 @@ CREATE TABLE public.user_logins
 
 insert into user_logins values ('person@gmail.com', 'person@gmail.com','person');
 
-insert into flight_details values('001','Air Canada','2019-06-28','11:30 AM','2019-06-25','1:30 PM','Victoria-YYJ','2','430','Vancouver-YVR','120'); 
+Flight Data:
+
+insert into flight_details values('001','Air Canada','2019-06-28','11:32','2019-06-25','13:30','Victoria-YYJ','2','430','Vancouver-YVR','120'); 
+insert into flight_details values('002','Air Transat','2019-06-28','11:30','2019-06-25','15:30','Victoria-YYJ','2','500','Edmonton-YEG','80'); 
+insert into flight_details values('003','WestJet','2019-06-29','9:13','2019-06-26','12:30','Ottawa-YOW','2','350','Toronto-YYZ','50'); 
+insert into flight_details values('004','Air Transat','2019-06-28','11:30','2019-06-26','13:30','Montreal-YUL','2','500','Ottawa-YOW','80'); 
+insert into flight_details values('005','WestJet','2019-06-29','9:13','2019-06-26','12:30','Halifax-YHZ','2','350','Toronto-YYZ','50'); 
+insert into flight_details values('006','Air Transat','2019-06-30','9:13','2019-06-29','12:30','Ottawa-YOW','2','350','Montreal-YUL','50'); 
 
